7ce1966da422302987e5388d4c34a103e314a201
guidetti <gustavooguidetti@gmail.com>
2023-03-16 22:02:35 -0300

    x

--- aula 03 - classes/lib/pratica/pratica02.dart
+++ aula 03 - classes/lib/pratica/pratica02.dart
@@ -1,20 +1,59 @@
-class Estado{
+class Estado {
   final String nome;
   final String sigla;
   final Function calcularPIB;
-  Estado({required this.nome,required this.sigla,
-  required this.calcularPIB});
+  Estado({required this.nome, required this.sigla, required this.calcularPIB});
 }
 
-class Cidade{
-  final String nome; 
+class Cidade {
+  final String nome;
   final Estado estado;
   final Function calcularDensidadeDemografica;
-  Cidade({required this.nome, required this.estado, 
-  required this.calcularDensidadeDemografica});
+  Cidade(
+      {required this.nome,
+      required this.estado,
+      required this.calcularDensidadeDemografica});
 }
 
-void main(List<String> arguments){
+void main(List<String> arguments) {
+
+  var cidade1 = Cidade(
+      nome: 'Floripa',
+      estado: Estado(
+          nome: 'Santa catarina',
+          sigla: 'SC',
+          calcularPIB: (){
+            var x = 5;
+            print('calculando pib');
+            print('${(3232 + 5454) / 590000}');
+          }
+          ),
+      calcularDensidadeDemografica: () {
+            var y = 43434343;
+            print('calculando densidade');
+            print('${50000 / 20000000}');
+          }
+      );
+
+ print("${cidade1.nome}");
+//-----------------------------------------------------------------------------------
+
+  var cidade = Cidade(
+      nome: 'Paranavaí',
+      estado: Estado(
+          nome: 'Paraná',
+          sigla: 'PR',
+          calcularPIB: () => print('${(3232 + 5454) / 590000}')
+          ),
+      calcularDensidadeDemografica: () {
+            print('calculando densidade');
+            print('${50000 / 20000000}');
+          }
+      );
+
+cidade.calcularDensidadeDemografica();
+
+
   /*
   exercício 01 - criar um objeto do tipo Cidade
   com referência anônima na interpolação da função print. 
@@ -34,6 +73,4 @@ void main(List<String> arguments){
   fórmula Densidade Demografica →  area / populacao;
   Paranavaí → area = 1202.3, populacao = 88374;
   */
-  
 }
-
